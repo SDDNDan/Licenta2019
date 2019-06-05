3c1842bff7d71321322c0e8363bc2b96d720486d
Dragos Slusariuc <Dragos.Slusariuc@centric.eu>
2019-06-05 09:50:35 +0300

    Create windows for UI

--- src/main/java/graphics/GeneticAlgorithmGraphics.java
+++ src/main/java/graphics/GeneticAlgorithmGraphics.java
@@ -1,4 +1,32 @@
 package graphics;
+
 import javafx.application.Application;
-public class GeneticAlgorithmGraphics extends Application  {
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GeneticAlgorithmGraphics extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public GeneticAlgorithmGraphics(){
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
